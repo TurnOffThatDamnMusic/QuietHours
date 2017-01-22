eff60ca91d0e726176de8c57eb18e04657d85227
Garrett Baski <garbaski@cs.du.edu>
2017-01-22 13:29:54 -0700

    some stuff:

---
+++ Description.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Description : MonoBehaviour {
+    public Text test;
+    public bool visible;
+
+	// Use this for initialization
+	void Start () {
+        test = GetComponent<Text>();
+        visible = false;
+        test.color = new Color(1, 1, 1, 0);
+        test.text = "SUPERTEST";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (visible)
+        {
+            test.color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            test.color = new Color(1, 1, 1, 0);
+        }
+    }
+
+    public void setText(string body)
+    {
+        test.text = body;
+    }
+}
